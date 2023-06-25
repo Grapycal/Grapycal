2413a82acea1d045f7cf995d29d6535f0fcdefe3
eri24816 <a931eric@gmail.com>
2023-06-25 22:41:43 +0800

    Make input field auto grow and shrink

--- frontend/src/sobjects/nodes/textInputNode.ts
+++ frontend/src/sobjects/nodes/textInputNode.ts
@@ -9,15 +9,16 @@ export class TextInputNode extends Node{
     `<div class="node block-node flex-horiz space-between">
         <div id="slot_input_port" class="no-width flex-vert space-evenly"></div>
         <div class="NodeContent full-width flex-horiz space-evenly">
-            <div id="label" class="center" >
-            </div><input id="input" type="text" style=" height:100%; text-align: center; border: none; outline: none; background: none;"/>
-                
+            <div id="label" class="center" ></div>
+            <input id="input" type="text" style=" min-width:4ch; height:100%; text-align: center; border: none; outline: none; background: none;"/>
+            <div id="fake_input"></div>
         </div>
         <div id="slot_output_port" class="no-width flex-vert space-evenly"></div>
     </div>`
     }
     text: StringTopic = null
     inputField: HTMLInputElement = null
+    fakeInput: HTMLElement = null
     compositioning: boolean = false
 
     constructor(objectsync: ObjectSyncClient, id: string) {
@@ -28,6 +29,17 @@ export class TextInputNode extends Node{
     protected onStart(): void {
         super.onStart()
         this.link(this.text.onSet, (text)=>{this.inputField.value = text})
+        this.fakeInput = this.htmlItem.getEl('fake_input')
+        this.fakeInput.style.position = 'absolute'
+        this.fakeInput.style.visibility = 'hidden'
+        this.fakeInput.style.font = this.inputField.style.font
+        this.fakeInput.style.fontSize = this.inputField.style.fontSize
+        this.fakeInput.style.fontFamily = this.inputField.style.fontFamily
+        //padding
+        this.fakeInput.style.setProperty('padding-left', window.getComputedStyle(this.inputField).getPropertyValue('padding-left'))
+        this.fakeInput.style.setProperty('padding-right', window.getComputedStyle(this.inputField).getPropertyValue('padding-right'))
+
+        this.resizeInput()
     }
 
     reshape(shape: string): void {
@@ -41,6 +53,7 @@ export class TextInputNode extends Node{
             if(!this.compositioning){
                 this.text.set(this.inputField.value)
             }
+            this.resizeInput()
         })
         this.inputField.addEventListener('compositionstart', (e)=>{
             this.compositioning = true
@@ -49,5 +62,16 @@ export class TextInputNode extends Node{
             this.compositioning = false
             this.text.set(this.inputField.value)
         })
+    }
+
+    private resizeInput(): void {
+        this.fakeInput.innerHTML = this.inputField.value
+        this.inputField.style.width = this.fakeInput.clientWidth + 'px'
+        for(let port of this.in_ports){
+            port.moved.invoke()
+        }
+        for(let port of this.out_ports){
+            port.moved.invoke()
+        }
     }
 }
