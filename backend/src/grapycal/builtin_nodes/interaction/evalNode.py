14409a83877f66573aedec2af4c815904f6eacde
eri24816 <a931eric@gmail.com>
2023-09-12 01:58:32 +0800

    Migrate to ActionNode

--- backend/src/grapycal/builtin_nodes/interaction/evalNode.py
+++ backend/src/grapycal/builtin_nodes/interaction/evalNode.py
@@ -1,32 +1,26 @@
+from grapycal.extension.utils import NodeInfo
+from grapycal.sobjects.activeNode import ActiveNode
 from grapycal.sobjects.controls import TextControl
 from grapycal.sobjects.edge import Edge
-from grapycal.sobjects.node import Node
 from grapycal.sobjects.port import InputPort, OutputPort
 
-class EvalNode(Node):
+class EvalNode(ActiveNode):
     category = 'interaction'
     
     def build_node(self):
+        super().build_node()
         self.out_port = self.add_out_port('')
-        self.expr_control = self.add_control(TextControl)
+        self.expr_control = self.add_control(TextControl,name='expr_control')
         self.label.set('Eval')
         self.shape.set('simple')
         self.expose_attribute(self.expr_control.text,'text',display_name='expression')
 
-    def init_node(self):
-        self.has_value = False
-        self.value = None
+    def recover_from_version(self, version: str, old: NodeInfo):
+        super().recover_from_version(version, old)
+        self.recover_controls('expr_control')
 
     def task(self):
         expression = self.expr_control.text.get()
         self.value = eval(expression,self.workspace.vars())
-        self.has_value = True
         for edge in self.out_port.edges:
-            edge.push_data(self.value)
-
-    def double_click(self):
-        self.run(self.task)
-
-    def output_edge_added(self, edge: Edge, port: OutputPort):
-        if self.has_value:
             edge.push_data(self.value)
