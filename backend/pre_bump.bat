2ca4799cf380a7c1aef753e8c336c96504001ed6
eri24816 <a931eric@gmail.com>
2023-10-29 18:33:33 +0800

    fix: fix syntax

--- backend/pre_bump.bat
+++ backend/pre_bump.bat
@@ -2,6 +2,6 @@ cd ../frontend
 CALL npm run build
 cd ..
 rm -rf backend/src/grapycal/webpage
-mkdir backend/src/grapycal/webpage
+mkdir "backend/src/grapycal/webpage" 
 cp -r frontend/dist/* backend/src/grapycal/webpage
 cd ..
