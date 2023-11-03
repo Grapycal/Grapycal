76af4e0036bc36b19cada5e26ddba61b77c55620
eri24816 <a931eric@gmail.com>
2023-11-03 15:37:13 +0800

    bump: version 0.6.0 → 0.6.1

--- backend/docs/source/conf.py
+++ backend/docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Grapycal Team'
 author = 'Grapycal Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.6.1'
 
 
 # -- General configuration ---------------------------------------------------
