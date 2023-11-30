fb81e0e19e1b31cc3cd85b95401dd3d6797e27c1
eri24816 <a931eric@gmail.com>
2023-11-30 22:09:07 +0800

    fix: fix ctrl c recreating workspace bug

--- backend/src/grapycal/app.py
+++ backend/src/grapycal/app.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 import shutil
 import signal
@@ -22,6 +23,7 @@ class GrapycalApp:
         if not config['path'].endswith('.grapycal'):
             config['path'] += '.grapycal'
 
+
     def run(self) -> None:
         """
         Server main loop
@@ -44,74 +46,80 @@ class GrapycalApp:
         workspace = None
         http_server = None
         # Here simply start one workspace in background
-        while True:
-            break_flag = False
-            try:
             
-                # Start webpage server
-                if not self._config['no_serve_webpage']:
-                    webpage_path = os.path.join(os.path.dirname(__file__),'webpage')
-                    print(f'Strating webpage server at localhost:{self._config["http_port"]} from {webpage_path}...')
-                    http_server = subprocess.Popen([sys.executable,'-m', 'http.server',str(self._config["http_port"])],
-                                     start_new_session=True,cwd=webpage_path,
-                                     stdout=subprocess.DEVNULL
-                                     )
+        # Start webpage server
+        if not self._config['no_serve_webpage']:
+            webpage_path = os.path.join(os.path.dirname(__file__),'webpage')
+            print(f'Strating webpage server at localhost:{self._config["http_port"]} from {webpage_path}...')
+            http_server = subprocess.Popen([sys.executable,'-m', 'http.server',str(self._config["http_port"])],
+                                start_new_session=True,cwd=webpage_path,
+                                stdout=subprocess.DEVNULL
+                                )
 
-                while True: # Restart workspace when it exits. Convenient for development
+        while True: # Restart workspace when it exits. Convenient for development
 
-                    # Start workspace
-                    print(f'Starting workspace {self._config["path"]} at {self._config["host"]}:{self._config["port"]}...')
+            with self._run_workspace() as workspace:
+                self._waitForWorkspace(workspace)
 
-                    workspace = subprocess.Popen([sys.executable,'-m', 'grapycal.core.workspace',
-                        '--port', str(self._config['port']),
-                        '--host', self._config['host'],
-                        '--path', self._config['path'],
-                        ],start_new_session=True)
-                    
-                    while True:
-                        time.sleep(1)
-                        if workspace.poll() is not None:
-                            print('Workspace exited with code',workspace.poll())
-                            break
+            restart = self._config['restart'] and workspace.poll() == 0
 
-                    restart = self._config['restart'] and workspace.poll() == 0
+            exit_message_file = f'grapycal_exit_message_{workspace.pid}'
+            if os.path.exists(exit_message_file):
+                with open(exit_message_file,'r') as f:
+                    exit_message = f.read()
+                os.remove(exit_message_file)
+            
+                for line in exit_message.split('\n'):
+                    if line == '':
+                        continue
+                    op, msg = line.split(' ',1)
+                    if op == 'open':
+                        self._config['path'] = msg
+                        self._config.save_settings()
+                        restart = True
 
-                    exit_message_file = f'grapycal_exit_message_{workspace.pid}'
-                    if os.path.exists(exit_message_file):
-                        with open(exit_message_file,'r') as f:
-                            exit_message = f.read()
-                        os.remove(exit_message_file)
-                    
-                        for line in exit_message.split('\n'):
-                            if line == '':
-                                continue
-                            op, msg = line.split(' ',1)
-                            if op == 'open':
-                                self._config['path'] = msg
-                                self._config.save_settings()
-                                restart = True
-
-                    if not restart:
-                        break
-
-            except KeyboardInterrupt:
-                print('Shutting down Grapycal server will force kill all workspaces. Press Ctrl+C again to confirm.')
-                try:
-                    time.sleep(3)
-                except KeyboardInterrupt:
-                    break_flag = True
-                else:
-                    print('Resumed')
-            if break_flag:
+            if not restart:
                 break
 
-        print('Stopping workspaces...')
-        if workspace:
-            workspace.send_signal(signal.SIGTERM)
-            workspace.wait()
+        print('Stopping http server...')
         if http_server:
             http_server.send_signal(signal.SIGTERM)
             http_server.wait()
         print('Grapycal server terminated')
         return
     
+
+    @contextlib.contextmanager
+    def _run_workspace(self):
+        """
+        Run a workspace. Ensure that the workspace is terminated when the context is exited.
+        """
+        print(f'Starting workspace {self._config["path"]} at {self._config["host"]}:{self._config["port"]}...')
+    
+        workspace = subprocess.Popen([sys.executable,'-m', 'grapycal.core.workspace',
+            '--port', str(self._config['port']),
+            '--host', self._config['host'],
+            '--path', self._config['path'],
+            ],start_new_session=True)
+        try:
+            yield workspace
+        finally:
+            workspace.send_signal(signal.SIGTERM)
+            workspace.wait()
+            print('Workspace terminated')
+
+    def _waitForWorkspace(self,workspace:subprocess.Popen):
+        while True:
+            try:
+                time.sleep(3)
+            except KeyboardInterrupt:
+                print('Shutting down Grapycal server will force kill all workspaces. Press Ctrl+C again to confirm.')
+                try:
+                    time.sleep(3)
+                except KeyboardInterrupt:
+                    return
+                else:
+                    print('Resumed')
+            if workspace.poll() is not None:
+                print('Workspace exited with code',workspace.poll())
+                return
