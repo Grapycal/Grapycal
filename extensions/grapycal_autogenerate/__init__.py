46b946ad865356df8fab6f52fa7751078275189d
hank93513 <hank93513@gmail.com>
2023-12-04 23:41:31 +0800

    feature: add function node generator

--- extensions/grapycal_autogenerate/__init__.py
+++ extensions/grapycal_autogenerate/__init__.py
@@ -1,7 +1,29 @@
-from grapycal.utils.nodeGen import MoudleNodeGenerator
+from grapycal.utils.nodeGen import MoudleNodeGenerator, FunctionNodeGenerator
 import requests
 
-requests_moudle = MoudleNodeGenerator(requests)
-requests_moudle.generate()
+# requests_moudle = MoudleNodeGenerator(requests)
+# requests_moudle.generate()
 
-globals().update(requests_moudle.node_types)
+
+
+# globals().update(requests_moudle.node_types)
+# 
+# requests_get_node = FunctionNodeGenerator(requests.get)
+# print(requests_get_node.node_types)
+# globals().update(requests_get_node.node_types)
+requests_get_node = FunctionNodeGenerator(requests.get)
+requests_get_node.generate()
+globals().update(requests_get_node.node_types)
+
+import time
+time_time_node = FunctionNodeGenerator(time.time)
+time_time_node.generate()
+globals().update(time_time_node.node_types)
+
+time_sleep_node = FunctionNodeGenerator(time.sleep)
+time_sleep_node.generate()
+globals().update(time_sleep_node.node_types)
+
+print_node = FunctionNodeGenerator(print)
+print_node.generate()
+globals().update(print_node.node_types)
