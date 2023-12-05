29a5f6c09972a2b0583da5d2eb01921a10cf4169
taro0513 <hank93513@gmail.com>
2023-12-05 16:24:23 +0800

    fix: add scroll to right side-bar

--- extensions/grapycal_autogenerate/__init__.py
+++ extensions/grapycal_autogenerate/__init__.py
@@ -1,35 +1,12 @@
-from grapycal.utils.nodeGen import MoudleNodeGenerator, FunctionNodeGenerator, ClassNodeGenerator
+from grapycal.utils.nodeGen import NoBuilitinFuncionGenerator
 import requests
+import json
 
-# requests_moudle = MoudleNodeGenerator(requests)
-# requests_moudle.generate()
+json_load_node = NoBuilitinFuncionGenerator(json.loads)
+json_loads_node = NoBuilitinFuncionGenerator(json.loads)
 
+json_load_node.generate()
+json_loads_node.generate()
 
+globals().update(json_load_node.node_types) 
 
-# globals().update(requests_moudle.node_types)
-# 
-# requests_get_node = FunctionNodeGenerator(requests.get)
-# print(requests_get_node.node_types)
-# globals().update(requests_get_node.node_types)
-requests_get_node = FunctionNodeGenerator(requests.get)
-requests_get_node.generate()
-globals().update(requests_get_node.node_types)
-
-import time
-time_time_node = FunctionNodeGenerator(time.time)
-time_time_node.generate()
-globals().update(time_time_node.node_types)
-
-time_sleep_node = FunctionNodeGenerator(time.sleep)
-time_sleep_node.generate()
-globals().update(time_sleep_node.node_types)
-
-print_node = FunctionNodeGenerator(print)
-print_node.generate()
-globals().update(print_node.node_types)
-
-import datetime
-
-datetime_datetime_node = ClassNodeGenerator(datetime.datetime)
-datetime_datetime_node.generate()
-globals().update(datetime_datetime_node.node_types)
