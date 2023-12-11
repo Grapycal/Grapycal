61c4a96d7034a2643d647911372c7c3291b88481
taro0513 <hank93513@gmail.com>
2023-12-11 18:58:05 +0800

    fix: update unchange code

--- backend/src/grapycal/utils/nodeGen.py
+++ backend/src/grapycal/utils/nodeGen.py
@@ -4,10 +4,6 @@ import re
 from typing import Any, Callable
 from types import ModuleType
 
-import requests
-requests.get
-
-
 def doc_prettify(doc: str) -> str:
     if doc is None:
         return ''
@@ -18,115 +14,6 @@ def doc_prettify(doc: str) -> str:
     doc = doc.replace('  ', '')
     doc = doc.replace('\t', '')
     return doc
-
-import time
-type(time)
-
-
-class MoudleNodeGenerator():
-    def __init__(self, cls, func_name = None):
-        self.cls = cls
-        self.class_name = cls.__name__
-        self.category = '{}'.format(self.class_name)
-        self.node_types = {}
-
-    
-    def generate(self):
-        all_functions = dir(self.cls)
-        filtered_functions = [func for func in all_functions if re.match(r'^[^_].*(?<![\\_]{2})$', func) and callable(getattr(self.cls, func))]
-        for func in filtered_functions:
-            if func in ['globals', 'hasattr', 'hash', 'help']:
-                continue
-            try:
-                print('Generating node for function: {}'.format(func))
-                print(self.cls)
-                try:
-                    _func_implement = getattr(self.cls, func)
-                except:
-                    continue
-                self._node_class_generator(_func_implement)
-                print('Generated node for function: {}'.format(func))
-            except Exception as e:
-                print('Failed to generate node for function: {}, Error => {}'.format(func, e))
-        
-    def _node_class_generator(
-        self,
-        func,
-        in_ports = ['*args', '**kwargs'],
-        out_ports = ['return'],
-        shape = 'simple',
-        css_classes = ['fit-content'],
-    ):
-        unsuport_built_in_function = False
-        unsuport_built_in_function_name = False
-        required_params = []     
-        try:
-            signature = inspect.signature(func)
-            params_values = signature.parameters.values()
-        except:
-            unsuport_built_in_function = True
-        
-        try:
-            func.__name__
-        except:
-            unsuport_built_in_function_name = True
-
-        if unsuport_built_in_function_name:
-            return
-
-        # if not params_values:
-        #     return
-        class _Node(FunctionNode):
-            category = self.category
-            if unsuport_built_in_function:
-                inputs = ['*args', '**kwargs']
-            elif in_ports != ['*args', '**kwargs']:
-                inputs = in_ports
-            else:
-                for param in params_values:
-                    if param.default == inspect.Parameter.empty:
-                        if param.kind == inspect.Parameter.VAR_POSITIONAL:
-                            required_params.append('*' + param.name)
-                        elif param.kind == inspect.Parameter.VAR_KEYWORD:
-                            required_params.append('**' + param.name)
-                        else:
-                            required_params.append(param.name)
-                inputs = required_params
-            max_in_degree = [1] * len(inputs)
-            outputs = out_ports
-            display_port_names = True
-
-            def build_node(self):
-                super().build_node()
-                self.label.set(func.__name__)
-                self.shape.set(shape)
-                self.__doc__ = func.__doc__ or 'Sorry, no docstring for this function.'
-                for css_class in css_classes:
-                    self.css_classes.append(css_class)
-
-            def calculate(self, **kwargs):
-                kwargs_key = None
-                args_key = None
-                print(kwargs)
-                for key, value in kwargs.items():
-                    if '**' in key:
-                        kwargs_in_kwargs = value
-                        kwargs_key = key
-                    elif '*' in key:
-                        args_in_kwargs = value
-                        args_key = key
-                    else:
-                        kwargs_in_kwargs = {}
-                        args_in_kwargs = []
-                if kwargs_key:
-                    del kwargs[kwargs_key]
-                if args_key:
-                    del kwargs[args_key]
-                return func(*args_in_kwargs, **kwargs, **kwargs_in_kwargs)
-            
-        _Node.__name__ = func.__name__ + 'Node'
-        # globals()[func.__name__] = _Node
-        self.node_types[func.__name__] = _Node
 
 class FunctionNodeGenerator():
     def __init__(self, func: Callable[..., Any], func_name: str | None = None, doc: str | None = None,
@@ -302,6 +189,7 @@ class ClassNodeGenerator():
                             self.in_ports.append(param.name)
                             if _modify_max_in_degree:
                                 self.max_in_degree.append(1)
+                            break
                         
                     # else:
                     #     self.in_ports.append(param.name)
@@ -377,16 +265,13 @@ class ClassNodeGenerator():
         _Node.__doc__ = _doc
         self.node_types[_class_node_type_name] = _Node
 
-class NoBuilitinFuncionGenerator:
+class FuncionNodeGenerator:
     def __init__(self, func: Callable[..., Any], func_name: str | None = None, doc: str | None = None,
                  category: str | None = None, in_ports: list[str] | None = None,
                  out_ports: list[str] | None = None, max_in_degree: list[int] | None = None,
                  shape: str | None = None, css_classes: list[str] | None = None,
-                 display_port_names: bool = True):
-        
-        # assert inspect.isbuiltin(func) == False, 'This function is a builtin function, please use FunctionNodeGenerator instead.'
-        # assert inspect.isfunction(func) == True, 'This function is not a function, please use others NodeGenerator instead.'
-
+                 display_port_names: bool = True,
+                 display_default_ports: int = None) -> FunctionNode:
         self.func = func
         self.func_name = func_name
         self.doc = doc
@@ -396,38 +281,58 @@ class NoBuilitinFuncionGenerator:
         self.max_in_degree = max_in_degree
         self.shape = shape
         self.css_classes = css_classes
-        self.node_types = dict()
         self.display_port_names = display_port_names
+        self.display_default_ports = display_default_ports
+
+        self.node_types = dict()
+        self.is_builtin = inspect.isbuiltin(self.func)
+
         self.initial_values()
 
     def initial_values(self):
         if self.func_name is None:
             self.func_name = self.func.__name__
+
         if self.category is None:
-            self.category = self.func.__module__
+            self.category = self.func.__module__.replace('.', '/')
+
         if self.max_in_degree is None:
             self.max_in_degree = []
             _modify_max_in_degree = True
         else:
             _modify_max_in_degree = False
+
+        try:
+            _signature = inspect.signature(self.func)
+            _parameters = _signature.parameters.values()
+            _all_parameters = str(_signature)[1:-1].split(', ')
+            _input_annotations = inspect.get_annotations(self.func)
+            _output_annotations = _signature.return_annotation
+            if '*' in _all_parameters:
+                _parameters = list(_parameters)
+                _parameters.insert(_all_parameters.index('*'), '*')
+        except:
+            _parameters = None
+
         if self.in_ports is None:
             self.in_ports = []
-            signature = inspect.signature(self.func)
-            params_values = signature.parameters.values()
-            for param in params_values:
-                if param.default == inspect.Parameter.empty:
-                    if param.kind == inspect.Parameter.VAR_POSITIONAL:
-                        self.in_ports.append('*' + param.name)
+            for _parameters in _signature:
+                if _parameters is '*':
+                    pass
+                elif _parameters.default == inspect.Parameter.empty:
+                    if _parameters.kind == inspect.Parameter.VAR_POSITIONAL:    
+                        self.in_ports.append('*' + _parameters.name)
                         if _modify_max_in_degree:
                             self.max_in_degree.append(64)
-                    elif param.kind == inspect.Parameter.VAR_KEYWORD:
-                        self.in_ports.append('**' + param.name)
+                    elif _parameters.kind == inspect.Parameter.VAR_KEYWORD:
+                        self.in_ports.append('**' + _parameters.name)
                         if _modify_max_in_degree:
                             self.max_in_degree.append(1)
                     else:
-                        self.in_ports.append(param.name)
+                        self.in_ports.append(_parameters.name)
                         if _modify_max_in_degree:
                             self.max_in_degree.append(1)
+
         if self.out_ports is None:
             self.out_ports = ['return']
         if self.shape is None:
@@ -541,7 +446,7 @@ class ModuleNodeGenerator(object):
         for _routin in self.routins:
             try:
                 print('Generating node for function: {}'.format(_routin[0]))
-                _func_node = NoBuilitinFuncionGenerator(_routin[1], _routin[0])
+                _func_node = FuncionNodeGenerator(_routin[1], _routin[0])
                 _func_node.generate()
                 self.node_types.update(_func_node.node_types)
                 print('Generated node for function: {}'.format(_routin[0]))
