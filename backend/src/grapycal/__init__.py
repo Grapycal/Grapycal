8b7d708f1209fb7d7aeca687c43662c2b5e628d3
eri24816 <a931eric@gmail.com>
2024-03-06 21:14:55 +0800

    bump: version 0.10.0 → 0.11.0

--- backend/src/grapycal/__init__.py
+++ backend/src/grapycal/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 from grapycal.app import GrapycalApp
 from grapycal.extension_api.utils import Bus
 from grapycal.sobjects.sourceNode import SourceNode
